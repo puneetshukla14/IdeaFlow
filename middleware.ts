6bbd34b7c511c25f49c52d98d0aef8746db610e9
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-08-05 09:47:30 +0530

    middleware errror resolved

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,25 @@
-// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
-// Copy the middleware code as it is from the above resource
+// middleware.ts
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-import { authMiddleware } from "@clerk/nextjs";
+// Specify which routes need protection
+const isProtected = createRouteMatcher([
+  "/dashboard(.*)",
+  "/profile(.*)",
+  "/ideas(.*)"
+]);
 
-export default authMiddleware({
-  // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk"],
-
-  // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtected(req)) {
+    await auth.protect(); // Will redirect unauthenticated users
+  }
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)"
+  ],
 };

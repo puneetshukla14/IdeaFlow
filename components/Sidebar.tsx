340392cbd721fbc89abaa4cfe6214ec98fbbaeae
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-08-07 16:29:05 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -224,7 +224,7 @@ export default function Sidebar() {
               {trialInfo}
             </span>
           )}
-        </div>
+        </div> 
       </div> 
     </SidebarWrapper>
   );     

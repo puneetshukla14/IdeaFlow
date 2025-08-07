f2a0a3d8e660d11c339d1a7b919cb3ca63b491c4
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-08-07 17:17:35 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@
 export default function Logo() {
   return (
     <div className="text-white font-semibold text-[17px] tracking-tight leading-none select-none">
-      Research<span className="text-neutral-400">Hub</span>
+      SSII<span className="text-neutral-400">Hub</span>
     </div>
   );
 }
